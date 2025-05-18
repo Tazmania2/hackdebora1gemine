2d7f4bea3b93756e4c9d87107c08b85b5c187669
Tazmania2 <TCRabelo@gmail.com>
2025-05-18 05:26:58 -0300

    Update app.routes.js

--- app/app.routes.js
+++ app/app.routes.js
@@ -10,27 +10,33 @@ angular.module('app')
       })
       .when('/login', {
         templateUrl: 'app/views/login/loginView.html',
-        controller:  'LoginController'
+        controller:  'LoginController',
+        controllerAs: 'vm'
       })
       .when('/register', {
         templateUrl: 'app/views/register/registerView.html',
-        controller:  'RegisterController'
+        controller:  'RegisterController',
+        controllerAs: 'vm'
       })
       .when('/dashboard', {
         templateUrl: 'app/views/dashboard/dashboardView.html',
-        controller:  'DashboardController'
+        controller:  'DashboardController',
+        controllerAs: 'vm'
       })
       .when('/profile', {
         templateUrl: 'app/views/profile/profileView.html',
-        controller:  'ProfileController'
+        controller:  'ProfileController',
+        controllerAs: 'vm'
       })
       .when('/rewards', {
         templateUrl: 'app/views/rewards/rewardsView.html',
-        controller:  'RewardsController'
+        controller:  'RewardsController',
+        controllerAs: 'vm'
       })
       .when('/virtual-goods', {
         templateUrl: 'app/views/virtual-goods/virtualGoodsView.html',
-        controller:  'VirtualGoodsController'
+        controller:  'VirtualGoodsController',
+        controllerAs: 'vm'
       })
       .otherwise({ redirectTo: '/login' });
   }]); 
