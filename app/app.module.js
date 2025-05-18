f2dbef0b3ae966ad393b3a6d200146238b2de337
Tazmania2 <TCRabelo@gmail.com>
2025-05-18 05:14:32 -0300

    GREAT SUCCESS

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,6 @@
-angular.module('app', ['ngRoute']); 
+angular.module('app', ['ngRoute'])
+  .run(['$rootScope', 'AuthService', function($rootScope, AuthService) {
+    $rootScope.isAuthenticated = function() {
+      return !!AuthService.getToken();
+    };
+  }]); 
--- app/auth.service.js
+++ app/auth.service.js
@@ -4,8 +4,21 @@ angular.module('app')
       this.getToken = function() {
         return localStorage.getItem('token');
       };
-      this.login = function(credentials) {
-        return $http.post(CONFIG.service + '/login', credentials);
+      this.login = function(email, password) {
+        var deferred = $q.defer();
+        $http.post(CONFIG.service + '/login', { email: email, password: password })
+          .then(function(response) {
+            if (response.data && response.data.token) {
+              localStorage.setItem('token', response.data.token);
+              deferred.resolve(response);
+            } else {
+              deferred.reject({ data: { message: 'Token não recebido do servidor.' } });
+            }
+          })
+          .catch(function(error) {
+            deferred.reject(error);
+          });
+        return deferred.promise;
       };
       this.handle401 = function() {
         $location.path('/login');
