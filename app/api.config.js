bd213e0dfe5d25f3e818bf06c9eca82af88fdf9e
Tazmania2 <TCRabelo@gmail.com>
2025-05-18 05:54:19 -0300

    fix reg

--- app/api.config.js
+++ app/api.config.js
@@ -2,5 +2,6 @@ angular.module('app')
   .constant('FUNIFIER_API_CONFIG', {
     clientId: '68252a212327f74f3a3d100d',
     apiKey:   '68252a212327f74f3a3d100d',
-    service:  'https://service2.funifier.com/v3'
+    service:  'https://service2.funifier.com/v3',
+    baseUrl:  'https://service2.funifier.com/v3'
   }); 
