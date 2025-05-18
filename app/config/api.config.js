213a39b0a3e6cfc9f86595e3756172ac8b3b4217
Tazmania2 <TCRabelo@gmail.com>
2025-05-18 03:54:18 -0300

    Update api.config.js

--- app/config/api.config.js
+++ app/config/api.config.js
@@ -7,5 +7,10 @@
         .constant('API_CONFIG', {
             apiKey: '68252a212327f74f3a3d100d',
             appSecret: '682605f62327f74f3a3d248e'
+        })
+        .constant('FUNIFIER_API_CONFIG', {
+            baseUrl: 'https://service2.funifier.com/v3',
+            apiKey: '68252a212327f74f3a3d100d',
+            appSecret: '682605f62327f74f3a3d248e'
         });
 })(); 
