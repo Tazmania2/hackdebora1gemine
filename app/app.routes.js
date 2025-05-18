1a9b8af55af105aac5a470d065cc5db575af4df6
Tazmania2 <TCRabelo@gmail.com>
2025-05-18 04:57:20 -0300

    Update app.routes.js

--- app/app.routes.js
+++ app/app.routes.js
@@ -12,29 +12,25 @@ angular.module('app')
         templateUrl: 'app/views/login/login.html',
         controller:  'LoginController'
       })
-      // .when('/', {
-      //   templateUrl: 'app/views/home.html',
-      //   controller:  'HomeController'
-      // })
-      // .when('/register', {
-      //   templateUrl: 'app/views/register/register.html',
-      //   controller:  'RegisterController'
-      // })
-      // .when('/dashboard', {
-      //   templateUrl: 'app/views/dashboard/dashboard.html',
-      //   controller:  'DashboardController'
-      // })
-      // .when('/profile', {
-      //   templateUrl: 'app/views/profile/profile.html',
-      //   controller:  'ProfileController'
-      // })
-      // .when('/rewards', {
-      //   templateUrl: 'app/views/rewards/rewards.html',
-      //   controller:  'RewardsController'
-      // })
-      // .when('/virtual-goods', {
-      //   templateUrl: 'app/views/virtual-goods/virtualGoods.html',
-      //   controller:  'VirtualGoodsController'
-      // })
+      .when('/register', {
+        templateUrl: 'app/views/register/register.html',
+        controller:  'RegisterController'
+      })
+      .when('/dashboard', {
+        templateUrl: 'app/views/dashboard/dashboard.html',
+        controller:  'DashboardController'
+      })
+      .when('/profile', {
+        templateUrl: 'app/views/profile/profile.html',
+        controller:  'ProfileController'
+      })
+      .when('/rewards', {
+        templateUrl: 'app/views/rewards/rewards.html',
+        controller:  'RewardsController'
+      })
+      .when('/virtual-goods', {
+        templateUrl: 'app/views/virtual-goods/virtualGoods.html',
+        controller:  'VirtualGoodsController'
+      })
       .otherwise({ redirectTo: '/login' });
   }]); 
