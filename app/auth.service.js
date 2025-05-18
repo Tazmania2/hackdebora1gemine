23ee393efd0c45b74dfa6ca6f6cd6284b3da9f43
Tazmania2 <TCRabelo@gmail.com>
2025-05-18 05:30:15 -0300

    Update auth.service.js

--- app/auth.service.js
+++ app/auth.service.js
@@ -6,18 +6,31 @@ angular.module('app')
       };
       this.login = function(email, password) {
         var deferred = $q.defer();
-        $http.post(CONFIG.service + '/login', { email: email, password: password })
-          .then(function(response) {
-            if (response.data && response.data.token) {
-              localStorage.setItem('token', response.data.token);
-              deferred.resolve(response);
-            } else {
-              deferred.reject({ data: { message: 'Token não recebido do servidor.' } });
-            }
-          })
-          .catch(function(error) {
-            deferred.reject(error);
-          });
+        $http({
+          method: 'POST',
+          url: CONFIG.service + '/auth/token',
+          headers: {
+            'Authorization': 'Basic NjgyNTJhMjEyMzI3Zjc0ZjNhM2QxMDBkOjY4MjYwNWY2MjMyN2Y3NGYzYTNkMjQ4ZQ==',
+            'Content-Type': 'application/json'
+          },
+          data: {
+            apiKey: CONFIG.apiKey,
+            grant_type: 'password',
+            username: email,
+            password: password
+          }
+        })
+        .then(function(response) {
+          if (response.data && response.data.access_token) {
+            localStorage.setItem('token', 'Bearer ' + response.data.access_token);
+            deferred.resolve(response);
+          } else {
+            deferred.reject({ data: { message: 'Token não recebido do servidor.' } });
+          }
+        })
+        .catch(function(error) {
+          deferred.reject(error);
+        });
         return deferred.promise;
       };
       this.handle401 = function() {
