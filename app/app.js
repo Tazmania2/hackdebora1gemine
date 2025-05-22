23b45bd346b4fc64915a032b617a64932857ce7f
Tazmania2 <TCRabelo@gmail.com>
2025-05-22 13:35:13 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -120,11 +120,10 @@
             })
             .when('/register-purchase', {
                 templateUrl: 'views/purchase/registerPurchaseView.html',
-                controller: 'RegisterPurchaseController',
+                controller: 'VirtualGoodsController',
                 controllerAs: 'vm',
                 resolve: {
                     auth: ['AuthService', function(AuthService) {
-                        console.log('[Route resolve] Checking authentication for /register-purchase');
                         return AuthService.isAuthenticated();
                     }]
                 }
