084cf18da18a2224f34ab4beaf59916a402149ce
Tazmania2 <TCRabelo@gmail.com>
2025-05-24 02:28:49 -0300

    fixing index

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ui.select'])
+angular.module('app', ['ui.select', 'ngSanitize'])
   .run(['$rootScope', 'AuthService', '$location', '$log', function($rootScope, AuthService, $location, $log) {
     $log = $log || console;
     $log.debug && $log.debug('[app.module.js] run block executed');
